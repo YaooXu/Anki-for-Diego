6adb6d22b764d3f846d76616833572e12901c451
顾骁 <bit1120172710@gmail.com>
2020-01-25 20:38:39 +0800

    get_word改为多线程

--- get_word.py
+++ get_word.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from PyQt5.QtWidgets import QMessageBox
+import threading
 
 
 def report_add_res(tot_num, success_num):
@@ -43,5 +44,50 @@ def get_word(word, timeout=5):
         return result
 
 
+class MyThread(threading.Thread):
+    def __init__(self, word, timeout=5):
+        super(MyThread, self).__init__()  # 重构run函数必须要写
+        self.word = word
+        self.timeout = timeout
+
+    def run(self):
+        r"""
+
+        :param word: 需要查询的单词
+        :param timeout: 时间限制
+        :return: 如果查询成功，返回该单词的信息的字典，否则返回None
+        {
+            "word": 单词 str
+            "img": 图片的网页链接 str
+            "st":例句 str
+            "sttr":例句翻译 str
+            "mean_cn": 中文释义 str
+            "accent": 音标 str
+            "sound": 发音的链接 str
+        }
+        """
+        url = u"http://mall.baicizhan.com/ws/search?w={word}".format(word=self.word)
+        print(url)
+        response = requests.get(url)
+        result = response.json()
+        if len(result) == 1:
+            result['errorCode'] = 1
+        else:
+            result['sound'] = "http://baicizhan.qiniucdn.com/word_audios/" + self.word + ".mp3"
+
+        print(result)
+        if result['errorCode'] == 1:
+            return None
+        else:
+            return result
+
+
 if __name__ == '__main__':
-    word_info = get_word('test')
+    #word_info = get_word('test')
+    t1=MyThread('test')
+    t2=MyThread('file')
+    t1.start()
+    t2.start()
+    #将t1,t2加入主线程
+    t1.join()
+    t2.join()
